08295e10977c96d9de0a93331cc6ac0bb8571e68
unstoppableWw <190559573@qq.com>
2023-12-26 11:07:27 +0800

    完成项目管理

--- src/api/project.js
+++ src/api/project.js
@@ -1,5 +1,32 @@
-import request from './request'
+import request from '@/utils/request'
 
-export async function getProjects() {
-  return await request.get('/api/project')
+// 新增项目
+export function addProject(data) {
+  return request({
+    url: '/api/project',
+    method: 'post',
+    data
+  })
+}
+
+// 获取项目
+export function getProject() {
+  return request.get('/api/project')
+}
+
+// 修改项目
+export function setProject(id, data) {
+  return request({
+    url: `/api/project/${id}`,
+    method: 'put',
+    data
+  })
+}
+
+// 删除项目
+export function delProject(id) {
+  return request({
+    url: `/api/project/${id}`,
+    method: 'DELETE'
+  })
 }
